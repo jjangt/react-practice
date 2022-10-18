6d22587425de35e2c2c474af0e17d755db4261af
tmjang <tmjang@mmonstar.co.kr>
2022-10-18 18:03:31 +0900

    테스트

---
+++ src/Test.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Test() {
+    return (
+        <>
+            <div>안녕히계세요</div>
+        </>
+    );
+}
+
+export default Test;
---
+++ src/test/Title.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Title() {
+    return (
+        <>
+            <h1>ㅎㅇㅎㅇㅎㅇ</h1>
+        </>
+    );
+}
+
+export default Title;
